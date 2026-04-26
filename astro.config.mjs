dd59e7e2e8683631e304d8c15836de9d7ec74a9a
민제 <minjae5474@naver.com>
2026-04-26 17:15:14 +0900

    Chore: Github Actions 배포 테스트

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://KOSSCCH.github.io",
-  base: "/kosscch-info-page",
+  base: "/Sandbox",
 });
